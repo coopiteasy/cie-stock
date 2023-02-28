5b29c82f17fd03f4075e309338f2842bdc7ed0c4
Victor Champonnois <victor@coopiteasy.be>
2023-02-28 12:24:28 +0100

    [ADD] purchase_stock_coverage (from beesdoo_purchase)

---
+++ purchase_stock_coverage/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Purchase Stock Coverage",
+    "summary": "Display product's stock coverage on purchase order line",
+    "author": "BEES coop - Cellule IT, Coop IT Easy SC",
+    "website": "https://coopiteasy.be",
+    "category": "Purchase",
+    "version": "12.0.1.0.0",
+    "depends": [
+        "purchase",
+        "product_stock_coverage",
+    ],
+    "data": [
+        "views/purchase_order.xml",
+    ],
+    "license": "AGPL-3",
+}
