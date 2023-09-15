dc0dfe513503ca3157cf0f2c423392d75526ce16
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2023-09-15 10:45:31 +0200

    [IMP] product_stock_coverage: black, isort, prettier

--- product_stock_coverage/tests/test_stock_coverage.py
+++ product_stock_coverage/tests/test_stock_coverage.py
@@ -32,12 +32,12 @@ class TestModule(TransactionCase):
     def test_compute_stock_coverage(self):
         self._create_order()
         self.pos_template._compute_stock_coverage()
-        self.assertEquals(1.0, self.pos_template.range_sales)
+        self.assertEqual(1.0, self.pos_template.range_sales)
         self.assertEqual(
             float_compare(0.0714, self.pos_template.daily_sales, precision_digits=2),
             0,
         )
-        self.assertEquals(210.0, self.pos_template.stock_coverage)
+        self.assertEqual(210.0, self.pos_template.stock_coverage)
 
     def _create_order(self):
         date = fields.Date.today() - timedelta(days=1)
@@ -45,7 +45,7 @@ class TestModule(TransactionCase):
         account = self.env.user.partner_id.property_account_receivable_id
         statement = self.pos_config.current_session_id.statement_ids[0]
         order_data = {
-            "id": u"0006-001-0010",
+            "id": "0006-001-0010",
             "to_invoice": True,
             "data": {
                 "pricelist_id": self.pricelist.id,
@@ -83,7 +83,7 @@ class TestModule(TransactionCase):
                 "creation_date": date_str,
                 "amount_tax": 0,
                 "fiscal_position_id": False,
-                "uid": u"00001-001-0001",
+                "uid": "00001-001-0001",
                 "amount_return": 0,
                 "sequence_number": 1,
                 "amount_total": 0.9,
